8851bf63f6320d792acce33cdb87ee931eeae2c4
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2019-07-22 11:03:26 +0200

    UIFC-56-Frontend-manages-filters SearchAndSort basics

---
+++ src/components/Filters/FilterView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+
+class FilterView extends React.Component {
+  static manifest = Object.freeze({
+    query: {},
+  });
+
+  render() {
+    return (
+      'hallo filter details'
+    );
+  }
+}
+
+export default FilterView;
--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,8 +1,118 @@
+import _ from 'lodash';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  injectIntl,
+  intlShape
+} from 'react-intl';
+import {
+  makeQueryFunction,
+  SearchAndSort
+} from '@folio/stripes/smart-components';
+import packageInfo from '../../../package';
 
-// export const filterConfig = [
-//   {
-//     name: FILTERS.TEST,
-//     cql: FILTERS.TEST,
-//     values: [],
-//   },
-// ];
+import FilterView from './FilterView';
+
+const INITIAL_RESULT_COUNT = 30;
+const RESULT_COUNT_INCREMENT = 30;
+
+const filterConfig = [
+  {
+    label: 'Type',
+    name: 'type',
+    cql: 'type',
+    values: [
+      { name: 'Whitelist', cql: 'Whitelist' },
+      { name: 'Blacklist', cql: 'Blacklist' }
+    ],
+  }
+];
+
+class Filters extends React.Component {
+  static manifest = Object.freeze({
+    initializedFilterConfig: { initialValue: false },
+    query: {
+      initialValue: {
+        query: '',
+        filters: '',
+        sort: 'label',
+      },
+    },
+    resultCount: { initialValue: INITIAL_RESULT_COUNT },
+    records: {
+      type: 'okapi',
+      records: 'fincSelectFilters',
+      recordsRequired: '%{resultCount}',
+      perRequest: 30,
+      path: 'finc-select/filters',
+      resourceShouldRefresh: true,
+      GET: {
+        params: {
+          query: makeQueryFunction(
+            'cql.allRecords=1',
+            '(label="%{query.query}*")',
+            {
+              'Filter Name': 'label'
+            },
+            filterConfig,
+            2,
+          ),
+        },
+        staticFallback: { params: {} },
+      },
+    }
+  });
+
+  static propTypes = {
+    resources: PropTypes.shape({
+      filters: PropTypes.shape({
+        records: PropTypes.arrayOf(PropTypes.object),
+      }),
+    }).isRequired,
+    mutator: PropTypes.shape({
+      filters: PropTypes.shape({
+        POST: PropTypes.func.isRequired,
+      }),
+      query: PropTypes.shape({
+        update: PropTypes.func,
+      }).isRequired,
+    }).isRequired,
+    stripes: PropTypes.object,
+    intl: intlShape.isRequired
+  };
+
+  render() {
+    const packageInfoReWrite = () => {
+      const path = '/finc-select/filters';
+      packageInfo.stripes.route = path;
+      packageInfo.stripes.home = path;
+      return packageInfo;
+    };
+
+    const { stripes, intl } = this.props;
+
+    return (
+      <div data-test-filter-instances>
+        <SearchAndSort
+          packageInfo={packageInfoReWrite()}
+          objectName="filter"
+          filterConfig={filterConfig}
+          initialResultCount={INITIAL_RESULT_COUNT}
+          resultCountIncrement={RESULT_COUNT_INCREMENT}
+          viewRecordComponent={FilterView}
+          visibleColumns={['label', 'type']}
+          viewRecordPerms="filters.item.get"
+          parentResources={this.props.resources}
+          parentMutator={this.props.mutator}
+          columnMapping={{
+            label: intl.formatMessage({ id: 'ui-finc-select.filter.label' }),
+            type: intl.formatMessage({ id: 'ui-finc-select.filter.type' })
+          }}
+          stripes={stripes}
+        />
+      </div>
+    );
+  }
+}
+
+export default injectIntl(Filters);
