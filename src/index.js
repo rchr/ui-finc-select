61362e66028c5e73e9ffb32535cae58af7a195e1
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2019-11-13 13:38:42 +0100

    UIFC-95-convert-to-SearchAndSortQuery adapt sources list of results

--- src/index.js
+++ src/index.js
@@ -2,42 +2,50 @@ import React from 'react';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import PropTypes from 'prop-types';
 import Switch from 'react-router-dom/Switch';
-import Route from 'react-router-dom/Route';
+
+import { Route } from '@folio/stripes/core';
+
+import SourcesRoute from './routes/SourcesRoute';
+// import SourceEditRoute from './routes/SourceEditRoute';
+// import SourceCreateRoute from './routes/SourceCreateRoute';
+// import SourceViewRoute from './routes/SourceViewRoute';
+// import CollectionsRoute from './routes/CollectionsRoute';
+// import CollectionViewRoute from './routes/CollectionViewRoute';
+// import CollectionCreateRoute from './routes/CollectionCreateRoute';
+// import CollectionEditRoute from './routes/CollectionEditRoute';
 
 import Settings from './settings';
-import Main from './Main';
 
 class FincSelect extends React.Component {
   static propTypes = {
-    stripes: PropTypes
-      .shape({ connect: PropTypes.func.isRequired })
-      .isRequired,
-    location: ReactRouterPropTypes.location.isRequired,
     match: ReactRouterPropTypes.match.isRequired,
     showSettings: PropTypes.bool,
-  }
-
-  /* set stripes.connect for manifest in MyDataClass (to get data from okapi) */
-  constructor(props, context) {
-    super(props, context);
-
-    this.connectedApp = props.stripes.connect(Main);
+    stripes: PropTypes.object.isRequired,
   }
 
   render() {
     if (this.props.showSettings) {
       return <Settings {...this.props} />;
     }
+
+    const { match: { path } } = this.props;
+
     return (
       <Switch>
-        <Route
-          path={`${this.props.match.path}`}
-          render={() => <this.connectedApp {...this.props} />}
-        />
-        <Route component={() => { this.NoMatch(); }} />
+        {/* <Route path={`${path}/metadata-sources/create`} component={SourceCreateRoute} />
+        <Route path={`${path}/metadata-sources/:id/edit`} component={SourceEditRoute} /> */}
+        <Route path={`${path}/metadata-sources/:id?`} component={SourcesRoute}>
+          {/* <Route path={`${path}/metadata-sources/:id`} component={SourceViewRoute} /> */}
+        </Route>
+        {/* <Route path={`${path}/metadata-collections/create`} component={CollectionCreateRoute} />
+        <Route path={`${path}/metadata-collections/:id/edit`} component={CollectionEditRoute} />
+        <Route path={`${path}/metadata-collections/:id?`} component={CollectionsRoute}>
+          <Route path={`${path}/metadata-collections/:id`} component={CollectionViewRoute} />
+        </Route> */}
       </Switch>
     );
   }
 }
 
 export default FincSelect;
+// export { default as MetadataSources } from './components/MetadataSources/MetadataSources';
