bc45558a0a52eec86c6ea6dc0d218b74217bbe15
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2019-12-02 13:34:28 +0100

    UIFC-101-bigtest add application tests

--- test/bigtest/tests/application-test.js
+++ test/bigtest/tests/application-test.js
@@ -1,27 +1,45 @@
-// import { beforeEach, describe, it } from '@bigtest/mocha';
-// import { expect } from 'chai';
+import { beforeEach, describe, it } from '@bigtest/mocha';
+import { expect } from 'chai';
 
-// import setupApplication from '../helpers/setup-application';
-// import ApplicationInteractor from '../interactors/application';
+import setupApplication from '../helpers/setup-application';
+import ApplicationInteractor from '../interactors/application';
 
-// describe('Application', () => {
-//   const app = new ApplicationInteractor();
+describe('Application', () => {
+  const app = new ApplicationInteractor();
 
-//   setupApplication();
+  setupApplication();
 
-//   beforeEach(function () {
-//     this.visit('/fincselect');
-//   });
+  beforeEach(function () {
+    this.visit('/finc-select');
+  });
 
-//   it('shows a greeting message', () => {
-//     expect(app.greetingMessage).to.equal('Congratulations!');
-//   });
+  // it('shows a greeting message', () => {
+  //   expect(app.greetingMessage).to.equal('Congratulations!');
+  // });
 
-//   it('has a link to examples', () => {
-//     expect(app.exampleLink).to.include('/fincselect/examples');
-//   });
+  // it('has a link to the developer guides', () => {
+  //   expect(app.guideLink).to.include('/dev-guide.md');
+  // });
 
-//   it('has a link to the developer guides', () => {
-//     expect(app.guideLink).to.include('/dev-guide.md');
-//   });
-// });
+  it('renders', () => {
+    expect(app.isPresent).to.be.true;
+  });
+
+  it('displays source-tab', () => {
+    expect(app.buttonSources).to.exist;
+  });
+
+  it('displays collection-tab', () => {
+    expect(app.buttonCollection).to.exist;
+  });
+
+
+  // it('show app title', () => {
+  //   expect(app.appTitle).to.equal('Finc Select');
+  // });
+
+  // describe('clicking the example button', () => {
+  //   beforeEach(async () => {
+  //     await examplePage.button.click();
+  //   });
+});
