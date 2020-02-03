b3f392f3b4d533ed6df07fd117d89d7b61a6ee2a
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-02-03 10:23:39 +0100

    UIFC-115-save-filters-bugfix

--- src/components/DisplayUtils/urls.js
+++ src/components/DisplayUtils/urls.js
@@ -6,9 +6,9 @@ const urls = {
   collectionView: id => `/finc-select/metadata-collections/${id}`,
 
   // showAllCollections: (sourceId) => `/finc-select/metadata-collections?filters=mdSource.${sourceId}`,
-  showAllCollections: (sourceId) => `/finc-select/metadata-collections?filters=mdSource.${sourceId},selected.yes,selected.no,permitted.yes,permitted.no`,
+  showAllCollections: (sourceId) => `/finc-select/metadata-collections?filters=mdSource.${sourceId},selected.yes,selected.no,permitted.yes,permitted.no,freeContent.undetermined,freeContent.no,freeContent.yes`,
   // showSelectedCollections: (sourceId) => `/finc-select/metadata-collections?filters=mdSource.${sourceId},selected.yes`,
-  showSelectedCollections: (sourceId) => `/finc-select/metadata-collections?filters=mdSource.${sourceId},selected.yes,permitted.yes,permitted.no`,
+  showSelectedCollections: (sourceId) => `/finc-select/metadata-collections?filters=mdSource.${sourceId},selected.yes,permitted.yes,permitted.no,freeContent.undetermined,freeContent.no,freeContent.yes`,
 
   filters: () => '/finc-select/filters',
   filterView: id => `/finc-select/filters/${id}`,
