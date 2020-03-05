d83631e98ae2abb048ec3df8a4da5088a6ad506e
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-03-05 13:59:17 +0100

    UIFC-139-add-collections-to-filter add renderList in view

--- src/components/Filters/Collections/CollectionsView.js
+++ src/components/Filters/Collections/CollectionsView.js
@@ -1,10 +1,60 @@
+import _ from 'lodash';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import {
+  List
+} from '@folio/stripes/components';
 
 class CollectionsView extends React.Component {
+  static propTypes = {
+    listedPermissions: PropTypes.arrayOf(PropTypes.object),
+    stripes: PropTypes.shape({
+      hasPerm: PropTypes.func.isRequired,
+      config: PropTypes.shape({
+        showPerms: PropTypes.bool,
+        listInvisiblePerms: PropTypes.bool,
+      }).isRequired,
+    }).isRequired,
+  };
+
+  renderList() {
+    const {
+      stripes,
+      listedPermissions,
+    } = this.props;
+    const showPerms = _.get(stripes, ['config', 'showPerms']);
+    const listFormatter = item => ((
+      <li key={item.permissionName}>
+        {
+          (showPerms ?
+            `${item.permissionName} (${item.displayName})` :
+            (item.displayName || item.permissionName))
+        }
+      </li>
+    ));
+    const noPermissionsFound = <FormattedMessage id="ui-finc-select.filter.collections.empty" />;
+
+    return (
+      <List
+        items={(listedPermissions || []).sort((a, b) => {
+          const key = showPerms ? 'permissionName' : 'displayName';
+          if (Object.prototype.hasOwnProperty.call(a, key) &&
+            Object.prototype.hasOwnProperty.call(b, key)) {
+            return (a[key].toLowerCase() < b[key].toLowerCase() ? -1 : 1);
+          }
+          return 1;
+        })}
+        itemFormatter={listFormatter}
+        isEmptyMessage={noPermissionsFound}
+      />
+    );
+  }
+
   render() {
     return (
       <React.Fragment>
-        here the connected collections will be shown
+        {this.renderList()}
       </React.Fragment>
     );
   }
