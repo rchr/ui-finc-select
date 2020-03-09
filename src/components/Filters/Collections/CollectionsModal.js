2bdad4f4b909dcdbc9fa4aa9998aff092afbe1dc
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-03-09 14:15:09 +0100

    UIFC-139-add-collections-to-filter show list of available collections in modal

--- src/components/Filters/Collections/CollectionsModal.js
+++ src/components/Filters/Collections/CollectionsModal.js
@@ -1,11 +1,17 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import {
+  FormattedMessage,
+  injectIntl,
+  intlShape
+} from 'react-intl';
 
 import { stripesConnect } from '@folio/stripes/core';
 import {
   Button,
   Modal,
+  MultiColumnList,
   Pane,
   Paneset,
 } from '@folio/stripes/components';
@@ -14,12 +20,39 @@ import css from './CollectionsModal.css';
 
 class CollectionsModal extends React.Component {
   static propTypes = {
+    intl: intlShape.isRequired,
     open: PropTypes.bool.isRequired,
     onClose: PropTypes.func.isRequired,
+    availableCollections: PropTypes.arrayOf(PropTypes.object),
+  };
+
+  static defaultProps = {
+    availableCollections:  {},
+  };
+
+  getArrayElementsCommaSeparated = (array) => {
+    let formatted = '';
+
+    if (array && array.length) {
+      for (let i = 0; i < array.length; i += 1) {
+        formatted += (i > 0 ? '; ' : '') + array[i];
+      }
+    }
+    return formatted;
+  }
+
+  resultsFormatter = {
+    label: collection => collection.label,
+    mdSource: collection => _.get(collection, 'mdSource.name', '-'),
+    permitted: collection => collection.permitted,
+    selected: collection => collection.selected,
+    filters: collection => this.getArrayElementsCommaSeparated(collection.filters),
+    freeContent: collection => collection.freeContent,
   };
 
   render() {
-    const { open, onClose } = this.props;
+    const { intl, open, onClose, availableCollections } = this.props;
+
     return (
       <Modal
         open={open}
@@ -59,7 +92,30 @@ class CollectionsModal extends React.Component {
               defaultWidth="fill"
             >
               <div>
-                Pane content
+                <MultiColumnList
+                  columnMapping={{
+                    label: intl.formatMessage({ id: 'ui-finc-select.collection.label' }),
+                    mdSource: intl.formatMessage({ id: 'ui-finc-select.collection.mdSource' }),
+                    permitted: intl.formatMessage({ id: 'ui-finc-select.collection.permitted' }),
+                    selected: intl.formatMessage({ id: 'ui-finc-select.collection.selected' }),
+                    filters: intl.formatMessage({ id: 'ui-finc-select.collection.filters' }),
+                    freeContent: intl.formatMessage({ id: 'ui-finc-select.collection.freeContent' })
+                  }}
+                  contentData={availableCollections}
+                  formatter={this.resultsFormatter}
+                  id="list-collections"
+                  // isEmptyMessage="no results"
+                  // isSelected={({ item }) => item.id === selectedRecordId}
+                  // onHeaderClick={onSort}
+                  // onRowClick={onSelectRow}
+                  // rowFormatter={this.rowFormatter}
+                  // sortDirection={
+                  //   sortOrder.startsWith('-') ? 'descending' : 'ascending'
+                  // }
+                  // sortOrder={sortOrder.replace(/^-/, '').replace(/,.*/, '')}
+                  // totalCount={count}
+                  visibleColumns={['label', 'mdSource', 'permitted', 'filters', 'freeContent']}
+                />
               </div>
             </Pane>
           </Paneset>
@@ -69,4 +125,4 @@ class CollectionsModal extends React.Component {
   }
 }
 
-export default stripesConnect(CollectionsModal);
+export default stripesConnect(injectIntl(CollectionsModal));
