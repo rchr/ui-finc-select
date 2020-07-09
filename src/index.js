e1893e70ae70739d81f14ad3e67245c5ebee30aa
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-07-09 10:47:13 +0200

    UIFC-154-add-credentials-to-settings

--- src/index.js
+++ src/index.js
@@ -18,30 +18,15 @@ import Settings from './settings';
 
 class FincSelect extends React.Component {
   static propTypes = {
-    actsAs: PropTypes.string.isRequired,
+    actAs: PropTypes.string.isRequired,
     match: ReactRouterPropTypes.match.isRequired,
-    showSettings: PropTypes.bool,
-    hasSettings: PropTypes.bool,
     stripes: PropTypes.object.isRequired,
   }
 
   render() {
-    // console.log('xxx');
-    // console.log(this.props.showSettings);
-    // console.log(this.props.hasSettings);
-    // console.log(this.props.actsAs);
+    const { actAs, match: { path } } = this.props;
 
-    if (this.props.showSettings) {
-      return <Settings {...this.props} />;
-    }
-
-    if (this.props.hasSettings) {
-      return <Settings {...this.props} />;
-    }
-
-    const { actsAs, match: { path } } = this.props;
-
-    if (actsAs === 'settings') {
+    if (actAs === 'settings') {
       return (
         <Settings {...this.props} />
       );
@@ -66,4 +51,3 @@ class FincSelect extends React.Component {
 }
 
 export default FincSelect;
-// export { default as MetadataSources } from './components/MetadataSources/MetadataSources';
