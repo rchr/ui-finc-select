a39ba2baadbf09a1883394ca18735e49fc998f7c
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-07-13 16:37:40 +0200

    UIFC-154-add-credentials-to-settings

--- src/settings/CredentialsSettings.js
+++ src/settings/CredentialsSettings.js
@@ -1,21 +1,68 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import {
+  Field
+} from 'redux-form';
+import {
+  TextField,
+  KeyValue,
+  Icon,
+  Layout,
+  Pane,
+  Row,
+  Col,
+} from '@folio/stripes/components';
+import { stripesConnect } from '@folio/stripes/core';
+import {
+  StripesConnectedSource
+} from '@folio/stripes/smart-components';
+import CredentialsSettingsView from './CredentialsSettingsView';
 
-import { ControlledVocab } from '@folio/stripes/smart-components';
-import { IntlConsumer } from '@folio/stripes/core';
+
+// import { ControlledVocab } from '@folio/stripes/smart-components';
+// import { IntlConsumer } from '@folio/stripes/core';
 
 class CredentialsSettings extends React.Component {
+  static manifest = Object.freeze({
+    ezbCredentials: {
+      type: 'okapi',
+      // records: 'fincSelectEZBCredentials',
+      records: 'ezbCredentials',
+      path: 'finc-select/ezb-credentials',
+      // resourceShouldRefresh: true
+    },
+  });
+
   static propTypes = {
+    children: PropTypes.node,
     stripes: PropTypes.shape({
-      connect: PropTypes.func.isRequired,
+      hasPerm: PropTypes.func.isRequired,
+      logger: PropTypes.object,
+    }),
+    ezbCredentials: PropTypes.object,
+    resources: PropTypes.object,
+    mutator: PropTypes.shape({
+      ezbCredentials: PropTypes.shape({
+        POST: PropTypes.func.isRequired,
+      }).isRequired,
     }).isRequired,
+    record: PropTypes.object,
   };
 
   constructor(props) {
     super(props);
 
-    this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
+    this.ezbCredentials = new StripesConnectedSource(this.props, this.logger, 'ezbCredentials');
+
+
+    console.log(this.props.resources);
+    // this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
+  }
+
+  componentDidMount() {
+    this.ezbCredential = new StripesConnectedSource(this.props, this.logger, 'ezbCredentials');
   }
 
   setRequiredValidation = (values) => {
@@ -31,34 +78,16 @@ class CredentialsSettings extends React.Component {
   }
 
   render() {
+    // const { ezbCredentials, stripes } = this.props;
+    const { record, stripes, children } = this.props;
+
+    console.log(this.ezbCredentials);
     return (
-      <IntlConsumer>
-        {intl => (
-          <this.connectedControlledVocab
-            baseUrl="finc-select/ezb-credentials"
-            columnMapping={{
-              user: intl.formatMessage({ id: 'ui-finc-select.settings.credentials.user' }),
-              password: intl.formatMessage({ id: 'ui-finc-select.settings.credentials.password' }),
-              libId: intl.formatMessage({ id: 'ui-finc-select.settings.credentials.libId' }),
-              isil: intl.formatMessage({ id: 'ui-finc-select.settings.credentials.isil' }),
-            }}
-            data-test-settings-finc-select-credentials
-            // hiddenFields={['description', 'numberOfObjects']}
-            id="fincSelectEZBCredentials"
-            label={<FormattedMessage id="ui-finc-select.settings.credentials.label" />}
-            labelSingular={<FormattedMessage id="ui-finc-select.settings.credentials.labelSingular" />}
-            nameKey="name"
-            objectLabel={<FormattedMessage id="ui-finc-select.settings.credentials.labelSingular" />}
-            records="fincSelectEZBCredentials"
-            sortby="name"
-            // validate={this.setRequiredValidation}
-            visibleFields={['user', 'password', 'libId', 'isil']}
-            {...this.props}
-          />
-        )}
-      </IntlConsumer>
+      <CredentialsSettingsView
+        ezbCredentials={_.get(this.props.resources)}
+      />
     );
   }
 }
 
-export default CredentialsSettings;
+export default stripesConnect(CredentialsSettings);
