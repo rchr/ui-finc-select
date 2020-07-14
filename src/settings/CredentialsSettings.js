ca6d07b790c1554296ffe4642345762b243a8598
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-07-14 11:28:24 +0200

    UIFC-154-add-credentials-to-settings

--- src/settings/CredentialsSettings.js
+++ src/settings/CredentialsSettings.js
@@ -3,9 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import {
-  Field
-} from 'redux-form';
-import {
   TextField,
   KeyValue,
   Icon,
@@ -14,56 +11,29 @@ import {
   Row,
   Col,
 } from '@folio/stripes/components';
-import { stripesConnect } from '@folio/stripes/core';
-import {
-  StripesConnectedSource
-} from '@folio/stripes/smart-components';
 import CredentialsSettingsView from './CredentialsSettingsView';
-
-
-// import { ControlledVocab } from '@folio/stripes/smart-components';
-// import { IntlConsumer } from '@folio/stripes/core';
 
 class CredentialsSettings extends React.Component {
   static manifest = Object.freeze({
     ezbCredentials: {
       type: 'okapi',
-      // records: 'fincSelectEZBCredentials',
-      records: 'ezbCredentials',
       path: 'finc-select/ezb-credentials',
       // resourceShouldRefresh: true
     },
   });
 
   static propTypes = {
-    children: PropTypes.node,
     stripes: PropTypes.shape({
       hasPerm: PropTypes.func.isRequired,
       logger: PropTypes.object,
     }),
-    ezbCredentials: PropTypes.object,
     resources: PropTypes.object,
     mutator: PropTypes.shape({
       ezbCredentials: PropTypes.shape({
         POST: PropTypes.func.isRequired,
       }).isRequired,
     }).isRequired,
-    record: PropTypes.object,
   };
-
-  constructor(props) {
-    super(props);
-
-    this.ezbCredentials = new StripesConnectedSource(this.props, this.logger, 'ezbCredentials');
-
-
-    console.log(this.props.resources);
-    // this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
-  }
-
-  componentDidMount() {
-    this.ezbCredential = new StripesConnectedSource(this.props, this.logger, 'ezbCredentials');
-  }
 
   setRequiredValidation = (values) => {
     const errors = {};
@@ -78,16 +48,14 @@ class CredentialsSettings extends React.Component {
   }
 
   render() {
-    // const { ezbCredentials, stripes } = this.props;
-    const { record, stripes, children } = this.props;
+    const { resources } = this.props;
 
-    console.log(this.ezbCredentials);
     return (
       <CredentialsSettingsView
-        ezbCredentials={_.get(this.props.resources)}
+        ezbCredentials={_.get(resources, 'ezbCredentials.records[0]')}
       />
     );
   }
 }
 
-export default stripesConnect(CredentialsSettings);
+export default CredentialsSettings;
