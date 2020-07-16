ffe006fdcfd7ff1b7586eb586075bc1bdd1c5660
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-07-16 10:28:33 +0200

    UIFC-154-add-credentials-to-settings reset configManager

--- src/settings/CredentialsSettings.js
+++ src/settings/CredentialsSettings.js
@@ -2,18 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { FormattedMessage } from 'react-intl';
-import {
-  TextField,
-  Row,
-  Col,
-} from '@folio/stripes/components';
-import { ConfigManager } from '@folio/stripes/smart-components';
-import { Field } from 'redux-form';
-
-// import CredentialsSettingsForm from './CredentialsSettingsForm';
-
-import { Required } from '../components/DisplayUtils/Validate';
+import CredentialsSettingsForm from './CredentialsSettingsForm';
 
 class CredentialsSettings extends React.Component {
   static propTypes = {
@@ -31,11 +20,11 @@ class CredentialsSettings extends React.Component {
       }).isRequired,
     }).isRequired,
     resources: PropTypes.object,
-    // mutator: PropTypes.shape({
-    //   ezbCredentials: PropTypes.shape({
-    //     POST: PropTypes.func.isRequired,
-    //   }).isRequired,
-    // }).isRequired,
+    mutator: PropTypes.shape({
+      ezbCredentials: PropTypes.shape({
+        POST: PropTypes.func.isRequired,
+      }).isRequired,
+    }).isRequired,
   };
   // static propTypes = {
   //   stripes: PropTypes.shape({
@@ -51,13 +40,12 @@ class CredentialsSettings extends React.Component {
     },
   });
 
-  // static defaultProps = {
-  //   handlers: {},
-  // }
+  static defaultProps = {
+    handlers: {},
+  }
 
   constructor(props) {
     super(props);
-    this.configManager = props.stripes.connect(ConfigManager);
 
     this.styles = {
       credentialsFormWrapper: {
@@ -75,121 +63,37 @@ class CredentialsSettings extends React.Component {
     return initialValues;
   }
 
-  // handleSubmit = (filter) => {
-  //   const { history, location, mutator } = this.props;
-  //   const collectionIdsForSave = filter.collectionIds;
-  //   // remove collectionIds for saving filter
-  //   const filterForSave = _.omit(filter, ['collectionIds']);
+  handleSubmit = (filter) => {
+    const { history, location, mutator } = this.props;
+    const collectionIdsForSave = filter.collectionIds;
+    // remove collectionIds for saving filter
+    const filterForSave = _.omit(filter, ['collectionIds']);
 
-  //   mutator.filters
-  //     .PUT(filterForSave)
-  //     .then(({ id }) => {
-  //       if (collectionIdsForSave) {
-  //         saveCollectionIds(id, collectionIdsForSave, this.props.stripes.okapi);
-  //       }
-  //       history.push(`${urls.filterView(id)}${location.search}`);
-  //     });
-  // }
-
-  beforeSave = (data) => {
-    const {
-      user,
-      password,
-      libId,
-      isil,
-    } = data;
-
-    return JSON.stringify({
-      user,
-      password,
-      libId,
-      isil,
-    });
+    mutator.ezbCredentials
+      .PUT(filterForSave)
+      .then(({ id }) => {
+        if (collectionIdsForSave) {
+          saveCollectionIds(id, collectionIdsForSave, this.props.stripes.okapi);
+        }
+        history.push(`${urls.filterView(id)}${location.search}`);
+      });
   }
 
   render() {
+    const { resources, stripes } = this.props;
+
     return (
       <div
         data-test-settings-ezb-credentials
         style={this.styles.credentialsFormWrapper}
       >
-        {/* <CredentialsSettingsForm
+        <CredentialsSettingsForm
           ezbCredentials={_.get(resources, 'ezbCredentials.records[0]')}
-          initialValues={this.getInitialValues()}
+          // initialValues={this.getInitialValues()}
+          initialValues={_.get(resources, 'ezbCredentials.records[0]')}
           onSubmit={this.handleSubmit}
           stripes={stripes}
-        /> */}
-        <this.configManager
-          getInitialValues={this.getInitialValues}
-          label="EZB credentials"
-          moduleName="FINC-CONFIG"
-          configName="ezb-credentials"
-          onBeforeSave={this.beforeSave}
-        >
-          <Row>
-            <Col xs={8}>
-              <Field
-                component={TextField}
-                fullWidth
-                id="addezbcredentials_user"
-                label={
-                  <FormattedMessage id="ui-finc-select.settings.ezbCredentials.user">
-                    {(msg) => msg + ' *'}
-                  </FormattedMessage>}
-                name="user"
-                placeholder="Enter a user"
-                validate={Required}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={8}>
-              <Field
-                component={TextField}
-                fullWidth
-                id="addezbcredentials_password"
-                label={
-                  <FormattedMessage id="ui-finc-select.settings.ezbCredentials.password">
-                    {(msg) => msg + ' *'}
-                  </FormattedMessage>}
-                name="password"
-                placeholder="Enter a password"
-                validate={Required}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={8}>
-              <Field
-                component={TextField}
-                fullWidth
-                id="addezbcredentials_libId"
-                label={
-                  <FormattedMessage id="ui-finc-select.settings.ezbCredentials.libId">
-                    {(msg) => msg + ' *'}
-                  </FormattedMessage>}
-                name="libId"
-                placeholder="Enter a libId"
-                validate={Required}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={8}>
-              <Field
-                component={TextField}
-                fullWidth
-                id="addezbcredentials_isil"
-                label={
-                  <FormattedMessage id="ui-finc-select.settings.ezbCredentials.isil">
-                    {(msg) => msg}
-                  </FormattedMessage>}
-                name="isil"
-                placeholder="Enter a isil"
-              />
-            </Col>
-          </Row>
-        </this.configManager>
+        />
       </div>
     );
   }
